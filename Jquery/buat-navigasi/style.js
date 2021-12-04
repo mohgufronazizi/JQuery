41b4073811bc695b8eda6427fa48bf7b0ac48d9d
mohgufronazizi <mohgufronazizi@gmail.com>
2021-12-04 19:26:42 +0700

    no message

---
+++ Jquery/buat-navigasi/style.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".menu-tab").click(function(){
+		
+		$(".menu-hide").toggleClass("show");
+		$("menu-tab").toggleClass("active");
+	});
+
+	$("a").click(function(){
+		$(".menu-hide").removeClass("show");
+		$(".menu-tab").removeClass("active");
+	});
+
+});
